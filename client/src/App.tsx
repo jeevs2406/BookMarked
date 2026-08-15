efa581c2a19b6701fadaefa1b2ffb74ecd53bd15
Jeevana <jeevs2406@gmail.com>
2026-08-15 16:10:34 +0800

    feat: add navigation bar to the top of the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { NavBar } from './components/NavBar';
 import { BrowseBooks } from './pages/BrowseBooks';
 
 function App() {
-  return <BrowseBooks />;
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-bg-primary">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/browse" replace />} />
+          <Route path="/browse" element={<BrowseBooks />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
